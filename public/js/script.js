39e80634ad05c5235217b69cb53b812277d62d31
03Sandraa <sandra@heijnekamp.nl>
2022-03-11 10:44:26 +0100

    Form functie verwijderd

--- public/js/script.js
+++ public/js/script.js
@@ -22,3 +22,9 @@ function openGebruikersmenu(){
 };
 
 /* ------ End animatie - Gekopieerd van eigen voorgaande project ------ */
+
+// const roepnaamtest = document.getElementById("froepnaam").value 
+// const omschrijvingtest = document.getElementById("fomschrijving").value
+// const roepnaam = document.getElementById("froepnaam").value = profiel["roepnaam"];
+// const omschrijving = document.getElementById("fomschrijving").value = {{{profiel.eigenProfiel.0.omschrijving}}};
+// const buttonOpslaan = document.querySelector(".bodyJouProfiel form button");
