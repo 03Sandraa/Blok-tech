16aebd5415794f0d05bde1a37259fa9606390d73
03Sandraa <sandra@heijnekamp.nl>
2022-03-17 19:07:27 +0100

    Formatting

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 /* ------ Animatie - Gekopieerd van eigen voorgaande project ------ */
 
 const buttonHamburgermenu = document.querySelector("header nav button");
+const navMenu = document.querySelector("header nav ul");
 
 buttonHamburgermenu.addEventListener("click", openGebruikersmenu);
 
@@ -10,14 +11,17 @@ function openGebruikersmenu() {
     if (HamburgermenuStatus == 0) {
         buttonHamburgermenu.classList.add("open");
         HamburgermenuStatus = 1;
+        navMenu.classList.remove("hidden")
     } else if (HamburgermenuStatus == 1) {
         buttonHamburgermenu.classList.remove("open");
         buttonHamburgermenu.classList.add("closed");
         HamburgermenuStatus = 2;
+        navMenu.classList.add("hidden")
     } else {
         buttonHamburgermenu.classList.add("open");
         buttonHamburgermenu.classList.remove("closed");
         HamburgermenuStatus = 1;
+        navMenu.classList.remove("hiddan");
     }
 };
 
